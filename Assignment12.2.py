7dcc23b284260161d67f418130e4a6b99a99adce
GajananThenge <38664939+GajananThenge@users.noreply.github.com>
2018-07-24 22:41:44 +0530

    Update Assignment12.2.py

--- Assignment12.2.py
+++ Assignment12.2.py
@@ -1,4 +1,4 @@
-'''2.​ Problem Statement
+'''2.â€‹ Problem Statement
 
 In this assignment students have to make ARIMA model over shampoo sales data and
 check the MSE between predicted and actual value.
@@ -28,9 +28,44 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 
 
+#Date parser
 def parser(x):
-    print(x)
     return datetime.strptime('190'+x, '%Y-%m')
-series = read_csv(r'C:\Users\hp-pc\Desktop/sales-of-shampoo-over-a-three-ye.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-autocorrelation_plot(series)
-pyplot.show()
+
+
+
+try:
+    #Get the dataset
+    series = read_csv(r'sales-of-shampoo-over-a-three-ye.csv', 
+                      header=0, parse_dates=[0], index_col=0, 
+                      squeeze=True, date_parser=parser)
+    X = series.values
+    
+    #Separates train and test data
+    size = int(len(X) * 0.66)
+    train, test = X[0:size], X[size:len(X)]
+    
+    
+    history = [x for x in train]
+    predictions = list()
+    for t in range(len(test)):
+        #Model creation and training
+    	model = ARIMA(history, order=(5,1,0))
+    	model_fit = model.fit(disp=0)
+        
+        #Model forecasting
+    	output = model_fit.forecast()
+    	yhat = output[0]
+    	predictions.append(yhat)
+    	obs = test[t]
+    	history.append(obs)
+    	print('predicted=%f, expected=%f' % (yhat, obs))
+    error = mean_squared_error(test, predictions)
+    print('Test MSE: %.3f' % error)
+    # plot
+    pyplot.plot(test,label='Actual')
+    pyplot.plot(predictions, color='red',label='Predicted')
+    pyplot.legend()
+    pyplot.show()
+except Exception as e:
+    print("Error Occured")
